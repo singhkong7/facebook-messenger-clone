da57b0e319a680d9de1a9c1966ed4d9a02b95126
singhkong7 <singhpriyansh2014@gmail.com>
2021-06-05 05:54:19 +0530

    Completed the messenger clone by adding iconbutton

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React,{forwardRef} from 'react';
 import "./Message.scss";
 import {Card,CardContent,Typography} from "@material-ui/core";
 
-function Message({message,username}) {
+const Message=forwardRef(({message,username}, ref) =>  {
     const isUser=username===message.username;
     return (
         
-           <div className={`message ${isUser  &&   'message__user'}`}>
+           <div ref={ref} className={`message ${isUser  &&   'message__user'}`}>
                 <Card className={isUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent>
                     <Typography
@@ -14,15 +14,15 @@ function Message({message,username}) {
                         variant="h5"
                         component="h2"
                     >
-                        {message.username}:{message.message}
+                        {isUser  &&   `${message.username || "Unknown User"}:`}{message.message}
                     </Typography>
                 </CardContent>
             </Card>
            </div>
            
     
-    )
-}
+    );
+})
 
 export default Message
 
--- src/messenger.js
+++ src/messenger.js
@@ -3,6 +3,9 @@ import "./messenger.scss";
 import { Button, FormControl,Input,InputLabel } from "@material-ui/core";
 import db from "./firebase/firebase";
 import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from '@material-ui/core';
 import Message from "./Message";
 function Messenger() {
   const [input, setInput] = useState(" ");
@@ -14,7 +17,7 @@ function Messenger() {
       .orderBy('timestamp','desc')
       .onSnapshot(snapshot=>
     {
-        setMessages(snapshot.docs.map(doc=>doc.data()))
+        setMessages(snapshot.docs.map(doc=>({id:doc.id,message:doc.data()})))
     });
   },[])
   useEffect(()=>
@@ -37,28 +40,33 @@ function Messenger() {
  
   return (
     <div className="Messenger">
+      <img  className="img" src="https://www.bing.com/th?id=OIP.dp9u3pkm7xUiTQKq4fuMlAHaDt&w=196&h=98&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt=" "></img>
         <h2>Welcome {username}</h2>
-      <FormControl>
-        <InputLabel>Enter a Message</InputLabel>
-        <Input
-          type="search"
-          placeholder="Type Your Message"
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <Button
-            disabled={!input}
-            variant="contained"
-            color="primary"
-            className="button"
-            type="submit"
-            onClick={sendMessage}
-        >
-          Send Message
-        </Button>
-      </FormControl>
-      {messages.map((message) => (
-        <Message username={username} message={message} />
-      ))}
+      <form className="Messenger_form">
+        <FormControl className="form-control">
+          <Input
+            className="input"
+            type="search"
+            placeholder="Type Your Message"
+            onChange={(event) => setInput(event.target.value)}
+          />
+            <IconButton
+              disabled={!input}
+              variant="contained"
+              color="primary"
+              className="Messenger_button"
+              type="submit"
+              onClick={sendMessage}
+            >
+              <SendIcon />
+            </IconButton>
+        </FormControl>
+      </form>
+        <FlipMove>
+            {messages.map(({id,message}) => (
+                <Message key={id} username={username} message={message} />
+            ))}
+        </FlipMove>
     </div>
   );
 }
